f44c203a77b4296cf4a3032187ea866d08aa64dc
ishukumar5663 <51350439+ishukumar5663@users.noreply.github.com>
2020-04-02 13:39:03 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notekeeper/screens/note_list.dart';
+import 'package:notekeeper/screens/note_detail.dart';
+
+void main() {
+	runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+	@override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+	    title: 'NoteKeeper',
+	    debugShowCheckedModeBanner: false,
+	    theme: ThemeData(
+		    primarySwatch: Colors.deepPurple,
+				brightness: Brightness.dark,
+				primaryColorDark: Colors.white,
+	    ),
+	    home: NoteList(),
+    );
+  }
+}
